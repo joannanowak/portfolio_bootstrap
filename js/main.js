cba70e993913191b506a4811a97b297d75815dc8
Joanna Nowak <j.traciak@gmail.com>
2016-12-18 14:58:19 +0100

    bootstrap portfolio

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(function () {
+
+	//	$(window).scroll(function(){
+	//		console.log($(window).scrollTop());
+	//		
+	//		if ($(window).scrollTop() >= 50){
+	//			$('#main-nav').css({'background-color': '#fff'});
+	//	 
+	//		}else{
+	//			$('#main-nav').css({'background-color': 'transparent'});
+	//		}	
+	//	});
+	//	
+
+
+	$(window).scroll(function () {
+
+
+		if ($(window).scrollTop() >= 50) {
+			$('#main-nav').addClass('scroll');
+
+		} else {
+			$('#main-nav').removeClass('scroll');
+		}
+	});
+
+	//	SMOOTH SCROLL
+	var root = $('body');
+	
+	$('a[href^="#"]').click(function () {
+		console.log("click");
+
+	var href = $(this).attr('href');	
+		
+		root.animate({
+			scrollTop: $(href).offset().top -52}, 500);
+	});
+
+
+
+});
